3cff17c2e2c0c92bd94a36c52e907892c099d1ac
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-02-05 18:44:34 +0530

    tenant debounce value change and current page

--- src/components/sections/Restaurants.tsx
+++ src/components/sections/Restaurants.tsx
@@ -60,9 +60,9 @@ const Restaurants = () => {
     const debouncedQueryUpdate = useMemo(() => {
         return debounce((key: string, value: string) => {
             setQueryParams((prev) => {
-                return { ...prev, [key]: value || "" }
+                return { ...prev, [key]: value || "", currentPage: 1 }
             })
-        }, 1000);
+        }, 500);
     }, [])
 
     const getFilterData = (key: string, value: string) => {
